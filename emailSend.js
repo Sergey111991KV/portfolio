cf8ef1ddc4f65b38be65e4d5bc858f4fabf038c0
Sergey <sergey.cosilov@gmail.com>
2022-07-17 10:37:04 +0300

    start add mail send

---
+++ emailSend.js
@@ -0,0 +1,23 @@
+import emailjs from 'emailjs-com'
+// import apiKeys from './apikeys'
+
+
+// const sendEmail = e => {
+//     console.log('sendEmail')
+//     // e.preventDefault()
+//     //
+//     // emailjs
+//     //     .sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+//     //     .then(
+//     //         result => {
+//     //             console.log(result.text)
+//     //         },
+//     //         error => {
+//     //             console.log(error.text)
+//     //         }
+//     //     )
+// }
+//
+// // sendEmail()
+
+console.log('hi')
